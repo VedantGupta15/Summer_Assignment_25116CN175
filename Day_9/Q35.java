3193f1c8b616ff3519f24ab786bf28cf5f426dc3
Vedant Gupta <guptavedant1545@gmail.com>
2026-06-05 21:07:26 +0530

    Day 9 complete

---
+++ Day_9/Q35.java
@@ -0,0 +1,18 @@
+package Day_9;
+
+public class Q35 {
+    public static void main(String[] args) {
+        int i,j;
+        char ch= 'A';
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(ch);
+                
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
